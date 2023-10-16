3b1182f42557ec3f3fc68ab31b4d8f632a51dffb
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-10-16 16:01:13 +0200

    added routes from react-router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Movies from "./Movies";
+import MovieDetail from "./MovieDetail";
 
 function App() {
   return (
-    <div>
-     <Header />
-     <Movies />
-     </div>
-    
+    <HashRouter>
+    <Header />
+    <Routes >
+      <Route path="/movieDetail" element={<MovieDetail />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/" element={<Movies />} />
+    </Routes>
+  </HashRouter>
   );
 }
 
