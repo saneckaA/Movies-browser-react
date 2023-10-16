ca915666f19cdfa5191a8340b5ad77c34012d92b
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-10-16 19:59:12 +0200

    fetched info about famous people

--- src/People/index.js
+++ src/People/index.js
@@ -1,41 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Title, FamousPeople, Person, Image, Name } from './styled';
+import axios from 'axios';
 
 const People = () => {
+
+    const [people, setPeople] = useState([]);
+
+    const fetchPeople = async () => {
+        try {
+            const response = await axios.get("https://api.themoviedb.org/3/person/popular?api_key=75950885b0db888f999efec40cdae6e8");
+            setPeople(response.data.results);
+            console.log(response.data.results);
+        } catch (error) {
+            console.error("Problem with fetching people data", error)
+        }
+    };
+
+    useEffect(() => {
+        fetchPeople();
+    }, []);
+
     return (
         <Container>
             <Title>
                 Popular people
             </Title>
             <FamousPeople>
-                <Person>
-                    <Image> <img src="images/poster.png" /></Image>
-                    <Name>Mulan</Name>
-                </Person>
-                <Person>
-                    <Image> <img src="images/poster.png" /></Image>
-                    <Name>Mulan</Name>
-                </Person>
-                <Person>
-                    <Image> <img src="images/poster.png" /></Image>
-                    <Name>Mulan</Name>
-                </Person>
-                <Person>
-                    <Image> <img src="images/poster.png" /></Image>
-                    <Name>Mulan</Name>
-                </Person>
-                <Person>
-                    <Image> <img src="images/poster.png" /></Image>
-                    <Name>Mulan</Name>
-                </Person>
-                <Person>
-                    <Image> <img src="images/poster.png" /></Image>
-                    <Name>Mulan</Name>
-                </Person>
-                <Person>
-                    <Image> <img src="images/poster.png" /></Image>
-                    <Name>Mulan</Name>
-                </Person>
+                {people.map((person) => (
+                    <>
+                        <Person key={person.id}>
+                            <Image> <img src={person.profile_path ? `https://image.tmdb.org/t/p/original${person.profile_path}` : "images/Profile.png"} /></Image>
+                            <Name>{person.name}</Name>
+                        </Person>
+                    </>
+                ))}
             </FamousPeople>
         </Container>
     )
