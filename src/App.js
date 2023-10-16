5e232721a99bbd9c21dae463c120b7b59a2a5dd3
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-10-16 20:08:49 +0200

    added person detail route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from "./Header";
 import Movies from "./Movies";
 import MovieDetail from "./MovieDetail";
 import People from "./People";
+import PersonDetail from "./PersonDetail";
 
 function App() {
   return (
     <HashRouter>
     <Header />
     <Routes >
+      <Route path="personDetail" element={<PersonDetail />} />
       <Route path="/people" element={<People />} />
       <Route path="/movieDetail" element={<MovieDetail />} />
       <Route path="/movies" element={<Movies />} />
