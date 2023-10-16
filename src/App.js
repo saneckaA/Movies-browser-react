dfaa107e974a8193cace02837b3453283f4c3033
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-10-16 19:42:02 +0200

    added poeple route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Movies from "./Movies";
 import MovieDetail from "./MovieDetail";
+import People from "./People";
 
 function App() {
   return (
     <HashRouter>
     <Header />
     <Routes >
+      <Route path="/people" element={<People />} />
       <Route path="/movieDetail" element={<MovieDetail />} />
       <Route path="/movies" element={<Movies />} />
       <Route path="/" element={<Movies />} />
