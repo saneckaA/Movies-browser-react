c60a21d5e87f66f7a5f3044f00268903db7e3649
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-10-19 12:30:23 +0200

    made new functions for fetching datas for movie

---
+++ src/MovieDetail/apiMovie.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { setCast, setCrew, setError, setMovie, setMovieSuccess } from "./movieSlice";
+
+const apiUrl = "https://api.themoviedb.org/3/";
+const apiKey = "75950885b0db888f999efec40cdae6e8";
+const imgUrl = "https://image.tmdb.org/t/p/original";
+
+export const fetchMovie = (movieId) => async (dispatch) => {
+    const url = `${apiUrl}movie/${movieId}?api_key=${apiKey}`;
+    dispatch(setMovie());
+        try {
+            const response = await axios.get(url);
+            console.log("Details:", response.data);
+            dispatch(setMovieSuccess(response.data));
+        } catch (error) {
+            dispatch(setError(error));
+        }
+    };
+
+export const fetchCredits = (movieId) => async (dispatch) => {
+    const url = `${apiUrl}movie/${movieId}/credits?api_key=${apiKey}`
+        try {
+            const response = await axios.get(url);
+            console.log("Credits:", response.data)
+            dispatch(setCast(response.data.cast));
+            dispatch(setCrew(response.data.crew));
+        } catch (error) {
+            dispatch(setError(error));
+        }
+    };
