ee0dd3a7188f449c967169cf4a3fc5ca67845bd5
Aleksandra Sanecka <a.saneckaa@gmail.com>
2023-10-19 14:44:53 +0200

    changed function to get genres for cast and crew in person detail component

---
+++ src/Genres/styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Types = styled.div`
+   display: flex;
+   flex-direction: row;
+   gap: 10px;
+   padding-top: 10px;
+   flex-wrap: wrap;
+`;
+
+export const Type = styled.div`
+   background: rgba(228, 230, 240, 1);
+   border-radius: 5px;
+   font-size: 14px;
+   font-weight: 400;
+   line-height: 19.6px;
+   padding: 8px 16px 8px 16px;
+`;
